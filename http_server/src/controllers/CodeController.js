b3081a92c504cb2efb65742083cfdd18ff0cf2ce
user1 <yongjuni30@gmail.com>
2024-05-22 00:27:31 +0900

    integrate cli, web & delete status

--- http_server/src/controllers/CodeController.js
+++ http_server/src/controllers/CodeController.js
@@ -1,22 +1,19 @@
-Job = require('../models/Job');
-JobProducer = require('../models/JobProducer');
+Job = require("../models/Job");
+JobProducer = require("../models/JobProducer");
 
-exports.send_to_refactoring = (req,res,next) => {
-    
-}
+exports.send_to_refactoring = (req, res, next) => {};
 //그냥 JobProducer를 Controller로 둘까
-//Job 클래스에 enqueue update_db, update_storage 메소드를 두고 
+//Job 클래스에 enqueue update_db, update_storage 메소드를 두고
 //Jobcontroller가 Job 생성하고 Job 클래스에 있는 메소드를 사용하는방식?
 
 //이렇게 하면 모델끼리 서로 얽힌다.
-exports.send_to_measure = (req,res,next) => {
-    //const id = 
-    //const code = req. ..
+exports.send_to_measure = (req, res, next) => {
+  //const id =
+  //const code = req. ..
 
-    job = new Job(id,code);
+  job = new Job(id, code);
 
-    JobProducer.enqueue(Job);
-    JobProducer.update_db(Job);
-    JobProducer.update_storage(Job)
-
-}
+  JobProducer.enqueue(Job);
+  JobProducer.update_db(Job);
+  JobProducer.update_storage(Job);
+};
