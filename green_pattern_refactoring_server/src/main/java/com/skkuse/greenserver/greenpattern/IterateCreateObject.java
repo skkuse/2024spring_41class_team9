bd62fd4e5f2507e1a2abbe3c941f066477096f4a
Suyeong <hsy3539@naver.com>
2024-05-24 01:10:29 +0900

    IterateCreateObject 원형 코드 최대한 유지 구현

--- green_pattern_refactoring_server/src/main/java/com/skkuse/greenserver/greenpattern/IterateCreateObject.java
+++ green_pattern_refactoring_server/src/main/java/com/skkuse/greenserver/greenpattern/IterateCreateObject.java
@@ -8,10 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Component
-public class IterateCreateObject implements CodeAnalyzer{
+public class IterateCreateObject implements CodeAnalyzer {
     @Override
     public String analyze(String buggyCode) {
-        int classStartIndex = 0;
         int objectCreationIndex = 0;
         int loopCreationIndex = 0;
 
@@ -28,15 +27,10 @@ public class IterateCreateObject implements CodeAnalyzer{
             // 객체 생성 확인
             for (int i = 0; i < lineSize; i++) {
                 String line = codes[i];
-                if (line.contains("public class Buggy")) {
-                    classStartIndex = i;
-                    continue;
-                }
 
                 Pattern objectPattern = Pattern.compile("\\b[A-Z]\\w*\\s+[a-z]\\w*\\s*=\\s*new\\s+[A-Z]\\w*\\s*\\(\\s*\\)\\s*;");
                 Matcher objectMatcher = objectPattern.matcher(line);
                 if (objectMatcher.find()) {
-                    System.out.println("Object creation detected: " + line);
                     objectCreationIndex = i;
                     break;
                 }
@@ -49,7 +43,6 @@ public class IterateCreateObject implements CodeAnalyzer{
                 Pattern forPattern = Pattern.compile("\\b(?:for|while)\\s*\\(.*?\\)\\s*\\{");
                 Matcher forMatcher = forPattern.matcher(line);
                 if (forMatcher.find()) {
-                    System.out.println("loop detected: " + line);
                     loopCreationIndex = i;
                     break;
                 }
@@ -57,11 +50,12 @@ public class IterateCreateObject implements CodeAnalyzer{
 
             // 수정
             if (objectCreationIndex != 0) {
-                lines.set(classStartIndex, "public class Fixed {\n");
                 String fixedContent = lines.get(objectCreationIndex);
+                int count = countLeadingSpaces(lines.get(objectCreationIndex));
+                String indent = " ".repeat(count);
 
                 lines.set(objectCreationIndex, "##MUSTDELETE##");
-                lines.add(loopCreationIndex, fixedContent);
+                lines.add(loopCreationIndex, indent + fixedContent);
 
                 lines.removeIf(item -> item.equals("##MUSTDELETE##"));
 
@@ -78,5 +72,17 @@ public class IterateCreateObject implements CodeAnalyzer{
         }
 
         return fixedCodeBuilder.toString();
+    }
+
+    private static int countLeadingSpaces(String text) {
+        int count = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
     }
 }
