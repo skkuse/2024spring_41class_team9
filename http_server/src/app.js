181f9301b45c6f009a6dc67c4e4d5fe09b71a9b4
user1 <yongjuni30@gmail.com>
2024-05-24 20:29:22 +0900

    test

--- http_server/src/app.js
+++ http_server/src/app.js
@@ -6,13 +6,9 @@ const app = express();
 
 //TODO:job producer에서 id발행, firebase 연동
 const user_router = require("./routes/User");
-const refactoring_router = require("./routes/Refactoring");
 
 //Web App and CLI App (user)
 app.use("/user", user_router);
-
-//Refactoring server
-app.use("/refactoring", refactoring_router);
 
 //run
 app.listen(3000, () => {
--- http_server/src/routes/Refactoring.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const refactoring = require("../controllers/CodeController");
-
-router.post("/refactoring_result", refactoring.send_to_refactoring);
-
-module.exports = router;
