f7de8a782fb86f40ff953db5c00acc19744456a7
user1 <yongjuni30@gmail.com>
2024-06-03 20:27:30 +0900

    completed except enqueue
    TODO: ENQUEUE
    
    aeng?

---
+++ app.js
@@ -0,0 +1,20 @@
+const https = require("https");
+const path = require("path");
+const express = require("express");
+require("dotenv").config();
+//const config = require("../util/config");
+
+const app = express();
+
+//TODO:job producer에서 id발행, firebase 연동
+const userRouter = require("./src/routes/User");
+
+app.use(express.json());
+
+//Web App and CLI App (user)
+app.use("/user", userRouter);
+
+//run
+app.listen(3000, () => {
+  console.log("running http server");
+});
