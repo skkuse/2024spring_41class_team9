e485da4a9a9607c6c20b9c98649123b40ea579a6
yoonjiii <yoonjian17@gmail.com>
2024-06-07 08:23:08 +0900

    worker implementation

--- job_worker/compile_worker.py
+++ job_worker/compile_worker.py
@@ -12,7 +12,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = pubsub_cred_path
 project_id = "swe-team9" # 프로젝트 이름
 subscription_id = "compileTopic-sub" # 구독 이름
 subscriber = pubsub_v1.SubscriberClient()
-timeout = 5.0
+timeout = 30.0
 
 # `projects/{project_id}/subscriptions/{subscription_id}`
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
@@ -29,6 +29,7 @@ if not firebase_admin._apps:
 storage_cred_path = 'swe-team9-feea36c92e8c.json'
 storage_client = storage.Client.from_service_account_json(storage_cred_path)
 
+
 def fetchJobMetadata(job_id):
     #job_id로 firestore에서 데이터 읽어오기 (code_path, binary_path, status)
     firestore_client = firestore.client()
@@ -40,28 +41,28 @@ def fetchJobMetadata(job_id):
         job_data = doc.to_dict()
         code_path = job_data.get('codePath', None)
         binary_path = job_data.get('binaryPath', None)
-        status = job_data.get('status', None)
+
+        doc_ref.update({
+            'status': "COMPILING"
+        })
         
         return {
             "code_path": code_path,
-            "binary_path": binary_path,
-            "status": status
+            "binary_path": binary_path
         }
     else:
         print('No such document!')
         return None
 
 def runCompileJob(code_path, binary_path):
-    # code_path:
-    # single java source code: ../MyClass.java
-    # java project: ../MyClass.java, ../My 
     code_path = code_path+"/"
+    print(code_path)
+
     bucket = storage_client.get_bucket('earth-saver')
     blobs = bucket.list_blobs(prefix=code_path)
 
     file_list = []
     for blob in blobs:
-        # 디렉터리 자체가 리스트에 포함되지 않도록 함
         if blob.name.endswith('/'):
             continue
         file_list.append(blob.name)
@@ -69,7 +70,8 @@ def runCompileJob(code_path, binary_path):
     for file in file_list:
         print(file)
     
-    if len(file_list) == 1: #single source code
+    if len(file_list) == 1 and file_list[0].endswith('.java'): #single source code
+        # Download project to local storage
         gcs_file_path = file_list[0]
         local_file_path = os.path.basename(gcs_file_path)
 
@@ -77,7 +79,7 @@ def runCompileJob(code_path, binary_path):
         blob.download_to_filename(local_file_path)
         print(f"Downloaded {gcs_file_path} to {local_file_path}")
 
-        # 컴파일 명령어 실행
+        # Compile
         compile_command = f"javac {local_file_path}"
         result = subprocess.run(compile_command, shell=True, text=True, capture_output=True)
         if result.returncode == 0:
@@ -88,51 +90,95 @@ def runCompileJob(code_path, binary_path):
         
         class_file_path = local_file_path.replace('.java', '.class')
         upload_path = binary_path + '/' + os.path.basename(class_file_path)
+        print(class_file_path)
+        print(upload_path)
         saveCompileResult(class_file_path, upload_path)
     else: # Java project
-        None
-    
+        print("project")
+
+        # Download project to local storage
+        for gcs_file_path in file_list:
+            local_file_path = 'code/' + gcs_file_path[len(code_path):]
+            local_directory = os.path.dirname(local_file_path)
+            if not os.path.exists(local_directory):
+                os.makedirs(local_directory)  # Ensure directory exists
+            blob = bucket.blob(gcs_file_path)
+            blob.download_to_filename(local_file_path)
+            print(f"Downloaded {gcs_file_path} to {local_file_path}")
+
+        # Modify build.gradle to make the JAR runnable
+        gradle_file = os.path.join('code','app', 'build.gradle')
+        with open(gradle_file, 'a') as file:
+            file.write('\njar {\n')
+            file.write('    manifest {\n')
+            file.write('        attributes("Main-Class": "demo.App")\n')
+            file.write('    }\n')
+            file.write('    from {\n')
+            file.write('        configurations.runtimeClasspath.collect { it.isDirectory() ? it : zipTree(it) }\n')
+            file.write('    }\n')
+            file.write('}\n')
+
+        # Use Gradle to build the project
+        print("Building project with Gradle...")
+        subprocess.run(['C:\\Gradle\\gradle-8.8\\bin\\gradle.bat', 'jar'], cwd=os.path.join('code', 'app'))
+        
+        # Upload the jar file
+        source_folder = os.path.join('code','app', 'build', 'libs')
+        for file_name in os.listdir(source_folder):
+            if file_name.endswith('.jar'):
+                jar_file_path = 'code/app/build/libs/' + file_name
+                upload_path = binary_path + '/' + file_name
+                print(jar_file_path)
+                print(upload_path)
+                saveCompileResult(jar_file_path, upload_path)
+
     return None
 
-def saveCompileResult(class_file_path, upload_path):
-    # 컴파일되어 현재 디렉토리에 만들어진 .class파일을 storage의 binary path에 저장
+def saveCompileResult(binary_file_path, upload_path):
+    # Save binary files to storage
     bucket = storage_client.get_bucket('earth-saver')
     blob = bucket.blob(upload_path)
-    blob.upload_from_filename(class_file_path)
-    print(f"Uploaded {class_file_path} to gs://earth-saver/{upload_path}")
+    blob.upload_from_filename(binary_file_path)
+    print(f"Uploaded {binary_file_path} to gs://earth-saver/{upload_path}")
 
 
 def measureJobPublish(project_id, topic_id, job_id):
     publisher = pubsub_v1.PublisherClient()
     topic_path = publisher.topic_path(project_id, topic_id)
-    
-    # job_id를 담은 message data를 만들어서 publish
-    # job_id가 string이라고 가정.
+
     data = job_id.encode("utf-8")
-    future = publisher.publish(topic_path, data)
-    print(future.result())
-    print(f"Published messages to {topic_path}.")
+    try:
+        future = publisher.publish(topic_path, data)
+        message_id = future.result()
+        print(f"Message published successfully with ID: {message_id}")
+        # status update
+        firestore_client = firestore.client()
+        doc_ref = firestore_client.collection('jobs').document(job_id)
+        doc_ref.update({
+            'status': "MEASURE_ENQUEUED"
+        })
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def subscribeAsync(message: pubsub_v1.subscriber.message.Message) -> None:
     # 1. message data에서 job_id알아내기
-    job_id = message.data.decode('utf-8')
+    job_id = message.data.decode('utf-8') 
     print(f"Job id: {job_id}")
 
-    # 2. fetchJobMetadata(job_id)
+    # 2. fetchJobMetadata(job_id) + status update to "COMPILING"
     job_metadata = fetchJobMetadata(job_id)
     if job_metadata:
         code_path = job_metadata.get('code_path')
         binary_path = job_metadata.get('binary_path')
-        status = job_metadata.get('status')
 
         # 3. Run compile job and save the result
         runCompileJob(code_path, binary_path)
 
-        # 4. Measure job publish
+        # 4. Measure job publish + status update to "MEASURE_ENQUEUED"
         measureJobPublish("swe-team9", "measureTopic", job_id)
-
-
+    
+    print("ack")
     message.ack()
 
 streaming_pull_future = subscriber.subscribe(subscription_path, callback=subscribeAsync)
--- job_worker/measure_worker.py
+++ job_worker/measure_worker.py
@@ -1,11 +1,13 @@
 import os
 from google.cloud import pubsub_v1
+from google.cloud import storage
 from concurrent.futures import TimeoutError
 import firebase_admin
 from firebase_admin import credentials, firestore, db
 import subprocess
 import psutil
 import time
+from system_values import get_system_values
 
 # Pub/Sub 설정
 pubsub_cred_path = 'swe-team9-ad44acd703b2.json'
@@ -13,76 +15,146 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = pubsub_cred_path
 project_id = "swe-team9" # 프로젝트 이름
 subscription_id = "measureTopic-sub" # 구독 이름
 subscriber = pubsub_v1.SubscriberClient()
-timeout = 5.0
+timeout = 15.0
 
 # `projects/{project_id}/subscriptions/{subscription_id}`
 subscription_path = subscriber.subscription_path(project_id, subscription_id)
 
 # Firebase 설정
 firebase_cred_path = 'swe-team9-5611bf21bb70.json'
-firebase_cred = credentials.Certificate(firebase_cred_path)
-firebase_admin.initialize_app(firebase_cred, {
-    'databaseURL': 'https://swe-team9-default-rtdb.firebaseio.com'
-})
+if not firebase_admin._apps:
+    firebase_cred = credentials.Certificate(firebase_cred_path)
+    firebase_admin.initialize_app(firebase_cred, {
+        'databaseURL': 'https://swe-team9-default-rtdb.firebaseio.com'
+    })
 
-def fetchJobMetadata(job_id):
-    #job_id로 firestore에서 데이터 읽어오기 (code_path, binary_path, status)
-    None
+# Storage 설정
+storage_cred_path = 'swe-team9-feea36c92e8c.json'
+storage_client = storage.Client.from_service_account_json(storage_cred_path)
 
 
-def runMeasureJob(binary_path):
-    command = ["java", binary_path]
 
-    process = psutil.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def fetch_job_metadata(job_id):
+    #job_id로 firestore에서 데이터 읽어오기 (binary_path)
+    firestore_client = firestore.client()
+    # jobs 컬렉션에서 job_id 문서 참조
+    doc_ref = firestore_client.collection('jobs').document(job_id)
+    # 문서 가져오기
+    doc = doc_ref.get() 
+    if doc.exists:
+        job_data = doc.to_dict()
+        binary_path = job_data.get('binaryPath', None)
+
+        doc_ref.update({
+            'status': "MEASURING"
+        })
+        
+        return binary_path
+    else:
+        print('No such document!')
+        return None
+
+
+        
+def run_measure_job(binary_path):
+    binary_path = binary_path+"/"
+
+    bucket = storage_client.get_bucket('earth-saver')
+    blobs = bucket.list_blobs(prefix=binary_path)
+
+    file_list = []
+    for blob in blobs:
+        if blob.name.endswith('/'):
+            continue
+        file_list.append(blob.name)
+    print("Files in directory:")
+    for file in file_list:
+        print(file)
+
+    if len(file_list) == 1 and file_list[0].endswith('.class'):
+        gcs_file_path = file_list[0]
+        local_file_path = os.path.basename(gcs_file_path)
+
+        # 확장자 제거
+        local_file_path = os.path.splitext(local_file_path)[0]
+
+        # Google Cloud Storage에서 파일 다운로드
+        blob = bucket.blob(gcs_file_path)
+        blob.download_to_filename(local_file_path + ".class")
+        print(f"Downloaded {gcs_file_path} to {local_file_path}.class")      
+
+        start_time = time.time()
+        result = subprocess.run(['java', local_file_path])
+        end_time = time.time()
+        runtime = end_time - start_time
+
+        print(f"runtime: {runtime} seconds")
+        if result.returncode == 0:
+            print("Execution successful.")
+        else:
+            print("Execution failed.")
+
+        return runtime
     
-    SLEEP_SECONDS = 0.1 # Sleep time in seconds between CPU and memory checks. Lower values will increase CPU usage then effect the monitoring result.
+    elif len(file_list) == 1 and file_list[0].endswith('.jar'):
+        gcs_file_path = file_list[0]
+        local_file_path = os.path.basename(gcs_file_path)
 
-    max_memory = 0
-    max_cpu = 0
-    start_time = time.time()
+        blob = bucket.blob(gcs_file_path)
+        blob.download_to_filename(local_file_path)
+        print(f"Downloaded {gcs_file_path} to {local_file_path}")
+        
+        # Run the generated JAR file
+        start_time = time.time()
+        result = subprocess.run(['java', '-jar', local_file_path])
+        end_time = time.time()
+        runtime = end_time - start_time
+        print(f"runtime: {runtime} seconds")
 
-    try:
-        while process.is_running():
-            current_memory = process.memory_info().rss
-            if current_memory > max_memory:
-                max_memory = current_memory
-            
-            # 전체 실행 시간 동안의 평균 CPU 사용량
-            # current_cpu = process.cpu_percent(interval=None) / psutil.cpu_count()
+        if result.returncode == 0:
+            print("Execution successful.")
+        else:
+            print("Execution failed.")
+        
+        return runtime
 
-            current_cpu = process.cpu_percent(interval=0.1) # 순간적인 최대 CPU 사용량
-            if current_cpu > max_cpu:
-                max_cpu = current_cpu
-            
-            time.sleep(SLEEP_SECONDS) # Sleep for a short period to avoid high CPU usage
+    else:
+        print(f"binary file error\n")
 
-    except psutil.NoSuchProcess:
-        pass
-    except psutil.AccessDenied:
-        print("Access Denied to process information")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+def cal_carbon_emission(runtime):
+    system_values = get_system_values()
+    return (runtime * (system_values['core_num'] * system_values['core_power'] + system_values['mem_num'] * system_values['mem_power']) * system_values['PUE'] * 0.001) * system_values['CI']
 
-    end_time = time.time()
-    runtime = end_time - start_time
-
-    # max_memory, max_cpu, runtime으로 carbon_emission 계산하기
-    carbon_emission = 0
-    return carbon_emission
-
-def saveMeasureResult(carbonEmission):
+def save_measure_result(carbon_emission, job_id):
+    # DB update
+    firestore_client = firestore.client()
+    doc_ref = firestore_client.collection('jobs').document(job_id)
+    doc_ref.update({
+        'carbonEmission': carbon_emission,
+        'status': "DONE"
+    })
     None
 
-def subscribeAsync(message: pubsub_v1.subscriber.message.Message) -> None:
+def subscribe_async(message: pubsub_v1.subscriber.message.Message) -> None:
     print(f"Received message: {message.data}")
     # 1. message data에서 job_id알아내기
-    # 2. binary_path = fetchJobMetadata(job_id)
-    # 3. carbon_emission = runMeasureJob(binary_path)
-    # 4. saveMeasureResult(carbon_emission)
+    job_id = message.data.decode('utf-8')
+    print(f"Job id: {job_id}")
+
+    # 2. binary_path = fetch_job_metadata(job_id)
+    binary_path = fetch_job_metadata(job_id)
+
+    if binary_path:
+        # 3. carbon_emission = run_measure_job(binary_path)
+        runtime = run_measure_job(binary_path)
+        carbon_emission = cal_carbon_emission(runtime)
+
+        # 4. save_measure_result(carbon_emission)
+        save_measure_result(carbon_emission, job_id)
 
     message.ack()
 
-streaming_pull_future = subscriber.subscribe(subscription_path, callback=subscribeAsync)
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=subscribe_async)
 print(f"Listening for messages on {subscription_path}..\n")
 
 # Wrap subscriber in a 'with' block to automatically call close() when done.
