44288ff2fb8ad21e8b1d1d5fab3a7213cad746e5
KYS <youngsuk0311@gmail.com>
2024-06-22 21:32:17 +0900

    sdk

--- cliapp/earthsaver/main.py
+++ cliapp/earthsaver/main.py
@@ -14,7 +14,7 @@ from rich.live import Live
 from rich.text import Text
 from rich.style import Style
 import time
-import keyboard 
+import keyboard
 import difflib
 from typing import Literal, TypedDict
 from enum import Enum
@@ -23,6 +23,8 @@ from firebase_admin import credentials, firestore
 import threading
 
 console = Console()
+
+# 전역함수
 
 # 로컬에서 모든 파일 불러와 list of dic 형태로 저장, 바이너리로 읽음
 def readFiles():
@@ -57,16 +59,16 @@ def encodingJavaCode(files_data):
 # HTTP 서버에 request를 보내는 함수. 명령어에 따라 엔드포인트와 리턴 구분
 def submitCode(request_body, Carbon):
     if Carbon:
-        url = 'https://http-server-dot-swe-team9.appspot.com/user/measure_carbonEmission' 
+        url = 'https://http-server-dot-swe-team9.appspot.com/user/measure_carbonEmission'
     else:
-        url = 'https://http-server-dot-swe-team9.appspot.com/user/refactoring_code' 
+        url = 'https://http-server-dot-swe-team9.appspot.com/user/refactoring_code'
         
     headers = {'Content-Type': 'application/json'}
     response = requests.post(url, headers=headers, json=request_body)
     response.raise_for_status()
- 
+
     try:
-        if Carbon:    
+        if Carbon:
             return response.json().get('job_id')
         else:
             return response.json()
@@ -84,7 +86,7 @@ class Code:
     loophandler = True # 모든 파일에 대해 diff가 끝나면 False
     current_index = 0 # 파일 하나씩 넘기기
     scroll_offset = 0 # 아래는 레이아웃 밑 스크롤 
-    max_scroll = 0
+    max_scroll = 0 # 화면이 너무 내려가지 않게 조절
     layout_height = 0
     layout_width = max(console.size.width/2 -5,1)
 
@@ -94,12 +96,11 @@ class Code:
         for file in Code.refactoredCode:
             if file["fileRelativePath"].endswith("java"):
                 Code.decodedRefactoredCode.append({
-                        "fileRelativePath": file["fileRelativePath"], #design doc 대로면 fileGreenB64가 맞는것 같은데 그냥 둬도 될거같아요
-                        "fileB64Decoded": base64.b64decode(file["fileB64Encoded"]) # base64.b64decode(file["fileGreenB64Encoded"])
+                        "fileRelativePath": file["fileRelativePath"], 
+                        "fileB64Decoded": base64.b64decode(file["fileB64Encoded"]) 
                     })
-#        console.print("Decode success", style="bold green")
 
-    # 리펙토링 명령어에 반응해 수행하는 중추 프로세스
+    # 리펙토링을 수행해 받아오는 헬퍼함수
     @staticmethod
     def codeRefactoring(request_body):
         console.print("Sending Project for Green Code...")
@@ -116,10 +117,9 @@ class Code:
         console.print("Refactoring Done!!", style="bold green")
 
 
-    # 화면에 diff를 띄우는 함수
+    # 화면에 diff하여 최종 화면을 출력하는 함수
     @staticmethod
     def displayDiff(file_memory):
-        #console.print("Getting Diff", style="bold blue")
         
         # 기존파일과 디코딩된 그린코드를 불러옴
         files_data = {file['fileRelativePath']: file['filememory'] for file in file_memory}
@@ -141,7 +141,7 @@ class Code:
             Code.firsttime = 0
 
         # 아래는 280 줄까지 화면출력
-        def add_real_line_number(lines):
+        def add_real_line_number(lines): # 각 줄의 맨 앞에 몇번째 줄인지 번호 붙이는 함수
             numbered_lines = []
             blank = 0
             for i, line in enumerate(lines):
@@ -154,17 +154,17 @@ class Code:
                 if line.startswith('!'):
                     numbered_lines.append("   /: ")
                     blank += 1
-            return numbered_lines         
+            return numbered_lines
         
-        def expand_tabs(line, tab_size=4):
+        def expand_tabs(line, tab_size=4): # indentation 통일시키는 함수. tab을 4개의 space로 쪼갬
             return line.replace('\t', ' ' * tab_size)
 
-        def wrap_lines_char(lines, width):
+        def wrap_lines_char(lines, width): # 화면 크기에 따라 줄을 잘라서 내리는 함수, 줄번호는 바뀌지 않음
             if not isinstance(width, int):
                 width = int(width)
             wrapped_lines = []
             for line in lines:
-                expanded_line = expand_tabs(line)   
+                expanded_line = expand_tabs(line)
                 if line[4]== ':':
                     for i in range(5, len(expanded_line), width-5):
                         if i == 5 :
@@ -191,7 +191,7 @@ class Code:
             return wrapped_lines
         
 
-        def display(file_path):
+        def display(file_path): # diff를 최종 화면에 맞게 구성하는 함수
             
             layout = Layout(name="root")
             layout.split_column(
@@ -206,7 +206,7 @@ class Code:
             original_content = files_data[file_path].decode('utf-8')
             refactored_content = decoded_data[file_path].decode('utf-8')
 
-            diff = list(difflib.ndiff(original_content.splitlines(), refactored_content.splitlines()))
+            diff = list(difflib.ndiff(original_content.splitlines(), refactored_content.splitlines())) # 양쪽 파일을 읽어서 diff시킴
 
 
             original_diff_lines = []
@@ -215,7 +215,7 @@ class Code:
             left_line_buffer = []
             right_line_buffer = []
 
-            for line in diff:
+            for line in diff: #diff 결과에서 +, -를 기준으로 왼쪽, 오른쪽, 혹은 양쪽에 줄을 추가
                 if line.startswith(' '):
                     # 같은 줄
                     original_diff_lines.extend(left_line_buffer)
@@ -239,9 +239,11 @@ class Code:
             original_diff_lines.extend(left_line_buffer)
             refactored_diff_lines.extend(right_line_buffer)
 
+            # 줄에 번호 붙이기
             numbered_origin_llines = add_real_line_number(original_diff_lines)
             numbered_refactored_llines = add_real_line_number(refactored_diff_lines)
 
+            # 화면 크기에 맞게 편집
             original_wrapped_lines = wrap_lines_char(numbered_origin_llines, Code.layout_width)
             refactored_wrapped_lines = wrap_lines_char(numbered_refactored_llines, Code.layout_width)
             
@@ -258,6 +260,7 @@ class Code:
             originalText = Text()
             refactorText = Text()
 
+            # 화면에 띄울 line들을 text에 추가
             for line in original_wrapped_lines[Code.scroll_offset:]:
                 if line[4] == ':':
                     originalText.append(line + '\n')
@@ -269,10 +272,12 @@ class Code:
                     refactorText.append(line + '\n')
                 elif line[4] == '+':
                     refactorText.append(line + '\n', style = Style(bgcolor="#003200"))
-     
+
+            # Text를 panel에 담기
             panel1 = Panel(originalText, title="Original Code", expand=True)
             panel2 = Panel(refactorText, title="Refactored Code", expand=True)
             
+            # 최종 화면구성
             layout["left"].update(panel1)
             layout["right"].update(panel2)
             footer_text = Text("Do you want to Accept change and Save? (y/n) ", style = "bold red")
@@ -308,10 +313,10 @@ class Code:
                 nextfile()
             if Code.loophandler == False :
                 console.print("Nothing changed")
-            if Code.loophandler == True:        
-                console.print("Getting Diff", style ="bold blue")   
-                time.sleep(1) 
-                with Live(display(file_paths[Code.current_index]), refresh_per_second=144, console=console, screen=True) as live:                
+            if Code.loophandler == True:
+                console.print("Getting Diff", style ="bold blue")
+                time.sleep(1)
+                with Live(display(file_paths[Code.current_index]), refresh_per_second=144, console=console, screen=True) as live:
                     while True:
                         if Code.loophandler == False:
                             live.stop()
@@ -322,6 +327,7 @@ class Code:
                             live.update(display(file_paths[Code.current_index]))
                         Code.layout_height = live.console.size.height
                         Code.layout_width = max(live.console.size.width/2 -5,1)
+            listener.stop()
 
         except KeyboardInterrupt:
             pass
@@ -329,16 +335,18 @@ class Code:
         for syntax in syntax_logs:
             console.print(syntax)
 
-# 미완성
+# 탄소배출량을 측정하는 Class
 class Carbon:
     carbonEmission = -0.1
-    job_id = ""
-    job_status = "COMPILE_ENQUEUED"
+    job_id = "" # http 서버로부터 받은 job id로 이를 통해 db polling함
+    job_status = "COMPILE_ENQUEUED" # 진행 상황에 따라 db로부터 다른 값을 받아와 갱신
     carbonCar = -0.1
     carbonPlane = -0.1
     carbonTree = -0.1
-    text = ""
+    text = "" #진행상황 출력용 text
     run_monitor = True
+
+    # 탄소배출량 측정을 위한 헬퍼함수
     @staticmethod
     def setCarbonEmission(request_body):
         console.print("Sending Project for Measuring...")
@@ -349,14 +357,13 @@ class Carbon:
             Carbon.dbPolling()
         else:
             console.print("Failed to start measuring.", style="bold red")
-# status에서 COMPILE_QUEUED가 안들어오고 skip되는 문제
+
+    # db로부터 값을 갱신해오는 함수
     @staticmethod
     def dbPolling():
         def on_snapshot(doc_snapshot, changes, read_time):
             try:
-                for doc in doc_snapshot:                 
-                    #data = doc.to_dict()  # 문서의 전체 데이터를 딕셔너리로 가져오기
-                    #print(f"Document data: {data}")  # 전체 데이터 출력   
+                for doc in doc_snapshot:
                     status = doc.get('status')
                     if status != "ERROR" and status != "COMPILE_ENQUEUED":
                         Carbon.text += " success!\n"
@@ -386,7 +393,7 @@ class Carbon:
 
         col_query = db.collection('jobs').document(Carbon.job_id)
         query_watch = col_query.on_snapshot(on_snapshot)
-        text_anime = [] 
+        text_anime = []
         text_anime.append(".")
         text_anime.append("..")
         text_anime.append("...")
@@ -405,10 +412,7 @@ class Carbon:
                 count += 1
                 count = count % 72
                 time.sleep(1/144)
-#            console.print("------")
-            live.stop() 
-
-#        query_watch.unsubscribe()
+            live.stop()
 
         if Carbon.job_status == "ERROR":
             console.print("ERROR!!",style = "bold red")
@@ -417,6 +421,7 @@ class Carbon:
 
 
 
+    # 측정값을 다양한 단위로 환산하여 최종 출력하는 함수
     @staticmethod
     def emissionConvert():
         Carbon.carbonCar = (Carbon.carbonEmission / 166.0)
@@ -425,7 +430,7 @@ class Carbon:
         console.print(f"\nYour project emits carbon {Carbon.carbonEmission:.6f} C g, same amount as",style = "bold green")
         console.print(f"Car: {Carbon.carbonCar:.6f} km / Plane: {Carbon.carbonPlane:.6f} km / Tree: {Carbon.carbonTree:.6f} 그루\n", style="bold green")
 
-# 명령어 생성
+# 명령어로 실행
 @click.command()
 @click.option('--measure','-m', is_flag=True, help="Measure the carbon footprint using Carbon class")
 @click.option('--refactor','-r', is_flag=True, help="Refactor the code using Code class")
@@ -446,7 +451,7 @@ def earthsaver(measure, refactor):
         if refactor:
             tasks.append(executor.submit(Code.codeRefactoring, request_body)) # 그린코드 생성
         
-        for task in tasks: #둘 다 끝나면 for문을 탈출
+        for task in tasks: #둘 다 끝나나서 출력단으로 진행
             task.result()
 
         if refactor:
